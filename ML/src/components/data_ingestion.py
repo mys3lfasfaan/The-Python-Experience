82947fc797567e4ba6ad679e4aca63a0d4056e8d
Asfaan Hussain <asfaanisworking@gmail.com>
2026-02-11 23:37:09 +0530

    UI with functionality

--- ML/src/components/data_ingestion.py
+++ ML/src/components/data_ingestion.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation = DataTransformation()
-    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
     
     model_training = ModelTrainer()
     print(model_training.initiate_model_trainer(train_arr, test_arr))
--- ML/src/logger.py
+++ ML/src/logger.py
@@ -1,11 +1,13 @@
 import logging
-import os
+import os, sys
 from datetime import datetime
 
 ## IMPORT THE LOGGER WHEREEVER YOU WANT TO USE !
+script_name = os.path.basename(sys.argv[0]) 
+log_filename = os.path.splitext(script_name)[0] + ".log"
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+LOG_FILE = f"{log_filename}-{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
